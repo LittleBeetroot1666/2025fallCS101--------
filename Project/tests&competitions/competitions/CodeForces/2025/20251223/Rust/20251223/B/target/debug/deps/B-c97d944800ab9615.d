867c3bb7642d6ceb23cc31754a689cc4833cd67f
LittleBeetroot1666 <19855701007@163.com>
2026-01-13 11:10:31 +0800

    Delete Project/tests&competitions/competitions/CodeForces/2025/20251223/Rust/20251223/B/target directory

--- Project/tests&competitions/competitions/CodeForces/2025/20251223/Rust/20251223/B/target/debug/deps/B-c97d944800ab9615.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lenovo\Desktop\rust_codes\competitions\Codeforces\20251223\B\target\debug\deps\B-c97d944800ab9615.d: src\main.rs
-
-C:\Users\Lenovo\Desktop\rust_codes\competitions\Codeforces\20251223\B\target\debug\deps\libB-c97d944800ab9615.rmeta: src\main.rs
-
-src\main.rs:
