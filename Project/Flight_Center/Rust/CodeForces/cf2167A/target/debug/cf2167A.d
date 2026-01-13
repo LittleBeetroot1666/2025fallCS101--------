99dc03b54ed1cfa5cba751820ce9e0fc3726f799
LittleBeetroot1666 <19855701007@163.com>
2026-01-13 11:07:32 +0800

    Delete Project/Flight_Center/Rust/CodeForces/cf2167A/target directory

--- Project/Flight_Center/Rust/CodeForces/cf2167A/target/debug/cf2167A.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Lenovo\Desktop\rust_codes\Flight_Center\CodeForces\cf2167A\target\debug\cf2167A.exe: C:\Users\Lenovo\Desktop\rust_codes\Flight_Center\CodeForces\cf2167A\src\main.rs
--- Project/Flight_Center/Rust/CodeForces/cf2167A/target/debug/deps/cf2167A.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lenovo\Desktop\rust_codes\Flight_Center\CodeForces\cf2167A\target\debug\deps\cf2167A.d: src\main.rs
-
-C:\Users\Lenovo\Desktop\rust_codes\Flight_Center\CodeForces\cf2167A\target\debug\deps\cf2167A.exe: src\main.rs
-
-src\main.rs:
