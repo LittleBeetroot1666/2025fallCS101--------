61b70c89373cf1aea08e56c3d9dc28d8c0a1dc73
LittleBeetroot1666 <19855701007@163.com>
2026-01-17 15:42:43 +0800

    Delete Project/assignments/assignment1 directory

--- Project/assignments/assignment1/root/Python/E02733.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-if n % 4 == 0 and n % 100 != 0:
-    print("Y")
-elif n % 400 == 0 and n % 3200 != 0:
-    print("Y")
-else:
-    print("N")
--- Project/assignments/assignment1/root/Python/E02750.py
+++
@@ -1,7 +0,0 @@
-a = int(input())
-if a % 2 == 0:
-    i = (a + 2) // 4
-    j = a // 2
-else:
-    i = j = 0
-print(i, j)
